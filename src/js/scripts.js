95f117adc6643521739accc026fc37c9a44554b0
WebNadin <nenadinweb@gmail.com>
2020-02-10 00:17:21 +0200

    before unstash

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,4 @@
 //= ../../node_modules/jquery/dist/jquery.min.js
-//= partials/helper.js
-//= partials/jquery.fancybox.min.js
-//= partials/jquery.psgTimer.js
-
 
 $(document).ready(function () {
     $('.nav a').each(function () {
